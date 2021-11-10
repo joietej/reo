1f15d39eaca817ecdcb8be5a5cf2681d0cbf9149
tejas sahasrabudhe <tejas.sahasrabudhe@gmail.com>
2021-11-10 18:08:52 +0530

    fix : lint issues

--- src/hooks/useFolders.ts
+++ src/hooks/useFolders.ts
@@ -1,4 +1,5 @@
 import useSWRV from "swrv";
+import { IResponse } from "swrv/dist/types";
 
 export type File = {
   id: string;
@@ -16,20 +17,24 @@ export type Folder = {
   files: File[];
 };
 
-export const isChecked = (id: string, selectedFiles: Array<File | null>) =>
-  selectedFiles.map((file) => file?.id).includes(id);
+export const isChecked = (
+  id: string,
+  selectedFiles: Array<File | null>
+): boolean => selectedFiles.map((file) => file?.id).includes(id);
 
-export const isVisited = (id: string, visitedFolderIds: Array<string>) =>
-  visitedFolderIds.includes(id);
+export const isVisited = (
+  id: string,
+  visitedFolderIds: Array<string>
+): boolean => visitedFolderIds.includes(id);
 
 export const searchFolder = (
   id: string | null,
   folder: Folder | null = null,
   root: Folder | null = null
-): Folder | any => {
+): Folder | null => {
   const source = folder?.folders || (root?.folders as Folder[]);
   if (id && source?.length) {
-    let parent = source.find((f) => f.id === id);
+    let parent = source.find((f) => f.id === id) || null;
     if (!parent) {
       for (let i = 0; i < source.length; i++) {
         parent = searchFolder(id, source[i]);
@@ -54,10 +59,9 @@ export const isValidFile = (file: File): boolean => {
   return false;
 };
 
-const useFolders = () => {
+const useFolders = (): IResponse<unknown, unknown> => {
   const url = "https://api-dev.reo.so/api/folderStructure ";
-  const { data, error } = useSWRV(url);
-  return { data, error };
+  return useSWRV(url);
 };
 
 export default useFolders;
